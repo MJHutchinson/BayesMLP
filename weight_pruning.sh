4e67dddf16209a8851f6cdf82408a7c0072c1627
mjhutchinson <hutchinson.michael.john@gmail.com>
2019-01-27 00:20:38 +0000

    Weight pruning from KL experiments

---
+++ weight_pruning.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+python parameter_sweep_regression.py -c ./config/weight_pruning/wine-quality-red.yaml           -ds wine-quality-red            -ld /scratch/mjh252/logs/ -dd /scratch/mjh252/data/UCL/ -cm weight_pruning -nd -g 0 1 2 3 &
+python parameter_sweep_regression.py -c ./config/weight_pruning/yacht.yaml                      -ds yacht                       -ld /scratch/mjh252/logs/ -dd /scratch/mjh252/data/UCL/ -cm weight_pruning -nd -g 0 1 2 3 &
+python parameter_sweep_regression.py -c ./config/weight_pruning/protein-tertiary-structure.yaml -ds protein-tertiary-structure  -ld /scratch/mjh252/logs/ -dd /scratch/mjh252/data/UCL/ -cm weight_pruning -nd -g 5 6 7 &
+
+wait
+echo "All Finished"
