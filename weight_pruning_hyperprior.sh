86318bd90d91a28d4857c57ac5a5200371aa5841
mjhutchinson <hutchinson.michael.john@gmail.com>
2019-02-07 13:03:16 +0000

    MNIST classification task bought up to date and pruning eperiments st up

--- weight_pruning_hyperprior.sh
+++ weight_pruning_hyperprior.sh
@@ -5,17 +5,18 @@
 #python parameter_sweep.py -c ./config/weight_pruning_hyperprior/yacht.yaml                      -ds yacht                        -ld /scratch/mjh252/logs/clean/ -dd /scratch/mjh252/data/presplit/ -cm weight_pruning_hyperprior -nd --gpu 0 1 2 3 &
 #python parameter_sweep.py -c ./config/weight_pruning_hyperprior/protein-tertiary-structure.yaml -ds protein-tertiary-structure   -ld /scratch/mjh252/logs/clean/ -dd /scratch/mjh252/data/presplit/ -cm weight_pruning_hyperprior -nd --gpu 0 1 2 3 &
 
-python parameter_sweep.py -c ./config/weight_pruning_hyperprior/concrete.yaml                   -ds concrete                     -ld /scratch/mjh252/logs/clean/ -dd /scratch/mjh252/data/presplit/ -cm weight_pruning_hyperprior -nd --gpu 0 1 2 3 &
-python parameter_sweep.py -c ./config/weight_pruning_hyperprior/energy.yaml                     -ds energy                       -ld /scratch/mjh252/logs/clean/ -dd /scratch/mjh252/data/presplit/ -cm weight_pruning_hyperprior -nd --gpu 0 1 2 3 &
-
-wait
-
-python parameter_sweep.py -c ./config/weight_pruning_hyperprior/kin8nm.yaml                     -ds kin8nm                       -ld /scratch/mjh252/logs/clean/ -dd /scratch/mjh252/data/presplit/ -cm weight_pruning_hyperprior -nd --gpu 0 1 2 3 &
-python parameter_sweep.py -c ./config/weight_pruning_hyperprior/naval-propulsion-plant.yaml     -ds naval-propulsion-plant       -ld /scratch/mjh252/logs/clean/ -dd /scratch/mjh252/data/presplit/ -cm weight_pruning_hyperprior -nd --gpu 0 1 2 3 &
-
-wait
-
-python parameter_sweep.py -c ./config/weight_pruning_hyperprior/power-plant.yaml                -ds power-plant                  -ld /scratch/mjh252/logs/clean/ -dd /scratch/mjh252/data/presplit/ -cm weight_pruning_hyperprior -nd --gpu 0 1 2 3 &
+#python parameter_sweep.py -c ./config/weight_pruning_hyperprior/concrete.yaml                   -ds concrete                     -ld /scratch/mjh252/logs/clean/ -dd /scratch/mjh252/data/presplit/ -cm weight_pruning_hyperprior -nd --gpu 0 1 2 3 &
+#python parameter_sweep.py -c ./config/weight_pruning_hyperprior/energy.yaml                     -ds energy                       -ld /scratch/mjh252/logs/clean/ -dd /scratch/mjh252/data/presplit/ -cm weight_pruning_hyperprior -nd --gpu 0 1 2 3 &
+#
+#wait
+#
+#python parameter_sweep.py -c ./config/weight_pruning_hyperprior/kin8nm.yaml                     -ds kin8nm                       -ld /scratch/mjh252/logs/clean/ -dd /scratch/mjh252/data/presplit/ -cm weight_pruning_hyperprior -nd --gpu 0 1 2 3 &
+#python parameter_sweep.py -c ./config/weight_pruning_hyperprior/naval-propulsion-plant.yaml     -ds naval-propulsion-plant       -ld /scratch/mjh252/logs/clean/ -dd /scratch/mjh252/data/presplit/ -cm weight_pruning_hyperprior -nd --gpu 0 1 2 3 &
+#
+#wait
+#
+#python parameter_sweep.py -c ./config/weight_pruning_hyperprior/power-plant.yaml                -ds power-plant                  -ld /scratch/mjh252/logs/clean/ -dd /scratch/mjh252/data/presplit/ -cm weight_pruning_hyperprior -nd --gpu 0 1 2 3 &
+python parameter_sweep.py -c ./config/weight_pruning_prior_hyperprior/mnist.yaml                -ds mnist                        -ld /scratch/mjh252/logs/clean/ -dd /scratch/mjh252/data/presplit/ -cm weight_pruning_prior_1 -nd --gpu 4 5 6 7 &
 
 
 wait
