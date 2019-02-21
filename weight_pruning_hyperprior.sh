a5f95aea575a4ae1b794c8deae4530cd02007ecd
mjhutchinson <hutchinson.michael.john@gmail.com>
2019-02-21 22:52:57 +0000

    Removed seed fixing for tensorflow

--- weight_pruning_hyperprior.sh
+++ weight_pruning_hyperprior.sh
@@ -2,19 +2,16 @@
 
 python parameter_sweep.py -c ./config/weight_pruning_hyperprior/wine-quality-red.yaml           -ds wine-quality-red             -ld /scratch/mjh252/logs/clean/ -dd /scratch/mjh252/data/presplit/ -cm weight_pruning_hyperprior3 -nd --gpu 0 1 2 3 &
 python parameter_sweep.py -c ./config/weight_pruning_hyperprior/bostonHousing.yaml              -ds bostonHousing                -ld /scratch/mjh252/logs/clean/ -dd /scratch/mjh252/data/presplit/ -cm weight_pruning_hyperprior3 -nd --gpu 0 1 2 3 &
-
-wait
-
 python parameter_sweep.py -c ./config/weight_pruning_hyperprior/yacht.yaml                      -ds yacht                        -ld /scratch/mjh252/logs/clean/ -dd /scratch/mjh252/data/presplit/ -cm weight_pruning_hyperprior3 -nd --gpu 0 1 2 3 &
-python parameter_sweep.py -c ./config/weight_pruning_hyperprior/protein-tertiary-structure.yaml -ds protein-tertiary-structure   -ld /scratch/mjh252/logs/clean/ -dd /scratch/mjh252/data/presplit/ -cm weight_pruning_hyperprior3 -nd --gpu 0 1 2 3 &
 
 wait
 
 python parameter_sweep.py -c ./config/weight_pruning_hyperprior/concrete.yaml                   -ds concrete                     -ld /scratch/mjh252/logs/clean/ -dd /scratch/mjh252/data/presplit/ -cm weight_pruning_hyperprior3 -nd --gpu 0 1 2 3 &
-python parameter_sweep.py -c ./config/weight_pruning_hyperprior/energy.yaml                     -ds energy                       -ld /scratch/mjh252/logs/clean/ -dd /scratch/mjh252/data/presplit/ -cm weight_pruning_hyperprior3 -nd --gpu 0 1 2 3 &
+python parameter_sweep.py -c ./config/weight_pruning_hyperprior/protein-tertiary-structure.yaml -ds protein-tertiary-structure   -ld /scratch/mjh252/logs/clean/ -dd /scratch/mjh252/data/presplit/ -cm weight_pruning_hyperprior3 -nd --gpu 0 1 2 3 &
 
 wait
 
+python parameter_sweep.py -c ./config/weight_pruning_hyperprior/energy.yaml                     -ds energy                       -ld /scratch/mjh252/logs/clean/ -dd /scratch/mjh252/data/presplit/ -cm weight_pruning_hyperprior3 -nd --gpu 0 1 2 3 &
 python parameter_sweep.py -c ./config/weight_pruning_hyperprior/kin8nm.yaml                     -ds kin8nm                       -ld /scratch/mjh252/logs/clean/ -dd /scratch/mjh252/data/presplit/ -cm weight_pruning_hyperprior3 -nd --gpu 0 1 2 3 &
 python parameter_sweep.py -c ./config/weight_pruning_hyperprior/naval-propulsion-plant.yaml     -ds naval-propulsion-plant       -ld /scratch/mjh252/logs/clean/ -dd /scratch/mjh252/data/presplit/ -cm weight_pruning_hyperprior3 -nd --gpu 0 1 2 3 &
 
